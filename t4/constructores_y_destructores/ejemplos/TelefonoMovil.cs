cb71769c1f67cae5ccaffa63d6e871e326f7922f
julcaso <julioenriquecastro@gmail.com>
2021-11-09 09:35:01 -0400

    Update TelefonoMovil.cs

--- t4/constructores_y_destructores/ejemplos/TelefonoMovil.cs
+++ t4/constructores_y_destructores/ejemplos/TelefonoMovil.cs
@@ -10,13 +10,12 @@ Materias: INF512, INF513.
 using System;
 class TelefonoMovil{
 
-/* 
-Atributos de clase.
-Debajo se encuentran las definiciones de los atributos de la clase TelefonoMovil.
-Realice los cambios en los atributos y metodos que sean necesarios para encapsular
-la clase TelefonoMovil.
-  
-*/
+    /* Atributos de clase, tambien llamados atributos de instancia.
+    Debajo se encuentran las definiciones de los atributos de la clase TelefonoMovil.
+    Realice los cambios en los atributos y metodos que sean necesarios para encapsular
+    la clase TelefonoMovil.
+    */
+    
     public string marca;
     public string color;
     public string modelo;
@@ -24,8 +23,11 @@ la clase TelefonoMovil.
     public int size;
     public string estado;
     
+    /* Constructores y destructores */
+
     //Constructor por defecto
     public TelefonoMovil(){
+        Console.WriteLine("Constructor sin argumentos. Tambien llamado constructor por defecto");
         marca = "iPhone";
         color = "Negro";
         modelo = "13";
@@ -36,25 +38,23 @@ la clase TelefonoMovil.
     
     //Constructor con argumentos
     public TelefonoMovil(string marca, string color){
+        Console.WriteLine("Constructor con argumentos.");
         this.marca = marca;
         this.color = color;
     }
 
-/* 
-Metodos de acceso: getters y setters.
-En este espacio defina los metodos get y set que sean necesarios para 
-encapsular la clase TelefonoMovil.
-*/
-
-    public void desplegarTelefonoMovil(){
-        Console.WriteLine("\n*** Salida ***\n");
-        Console.WriteLine("Marca:            " + marca);
-        Console.WriteLine("Color:            " + color);
-        Console.WriteLine("Modelo:           " + modelo);
-        Console.WriteLine("Cantidad Camaras: " + cantidadCamaras);
-        Console.WriteLine("Tamaño:           " + size);
-        Console.WriteLine("Estado:           " + estado);
+    //Destructor
+    ~TelefonoMovil(){
+        Console.WriteLine("Se invoca cuando se destruye el objeto");
     }
+    
+    /* Métodos de acceso: getters y setters.
+    En este espacio defina los metodos get y set que sean necesarios para
+    encapsular la clase TelefonoMovil.
+    */
+
+    
+    /* Métodos de clase, tambien llamados métodos de instancia.*/
 
     public bool marcar(){
         return true;
@@ -65,7 +65,6 @@ encapsular la clase TelefonoMovil.
     }
 
     public void vibrar(){
-
     }
 
     public bool encender(){
@@ -76,27 +75,39 @@ encapsular la clase TelefonoMovil.
         return true;
     }
 
+    public void desplegarInformacion(){
+        Console.WriteLine("\n*** Salida ***\n");
+        Console.WriteLine("Marca:            " + marca);
+        Console.WriteLine("Color:            " + color);
+        Console.WriteLine("Modelo:           " + modelo);
+        Console.WriteLine("Cantidad Camaras: " + cantidadCamaras);
+        Console.WriteLine("Tamaño:           " + size);
+        Console.WriteLine("Estado:           " + estado + "\n");
+    }
+
 }
+
+/* Clase de prueba, tambien llamada Tester Class. Es la que contiene el metodo main. */
 
 class TelefonoMovilTest{
     public static void Main(){
         
-        TelefonoMovil miCelular = new TelefonoMovil();
-      
-        miCelular.marca = "Samsung";
-        miCelular.color = "Negro";
-        miCelular.modelo = "A50";
-        miCelular.cantidadCamaras = 1000;
-        miCelular.size = 6;
-        miCelular.estado = "Sumergido en agua";
+        TelefonoMovil miCelular1 = new TelefonoMovil();
+
+        miCelular1.marca = "Samsung";
+        miCelular1.color = "Negro";
+        miCelular1.modelo = "A50";
+        miCelular1.cantidadCamaras = 1000;
+        miCelular1.size = 6;
+        miCelular1.estado = "Sumergido en agua";
         
-        miCelular.desplegarTelefonoMovil();
+        miCelular1.desplegarInformacion();
 
         TelefonoMovil miCelular2 = new TelefonoMovil();
-        miCelular2.desplegarTelefonoMovil();        
+        miCelular2.desplegarInformacion();
 
         TelefonoMovil miCelular3 = new TelefonoMovil("Xaomi", "Blanco");
-        miCelular3.desplegarTelefonoMovil();
+        miCelular3.desplegarInformacion();
 
     }
 }
