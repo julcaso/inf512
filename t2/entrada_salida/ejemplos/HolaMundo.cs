9d0a5c21b0d76a080f07f6c8ebff3c1cc13d2d93
julcaso <julioenriquecastro@gmail.com>
2022-02-28 20:23:58 -0400

    Update HolaMundo.cs

--- t2/entrada_salida/ejemplos/HolaMundo.cs
+++ t2/entrada_salida/ejemplos/HolaMundo.cs
@@ -1,11 +1,29 @@
-//HolaMundo.cs 
+//HolaMundo.cs
 
-using System; 
+//Declaración using.
+using System;
 
+//Declaracion de Clase en notación camellada.
 class HolaMundo
 {
+    //El metodo main.
     static void Main()
     {
-        Console.WriteLine("Hola, Mundo");
+        //Declaración de variables.
+        String matricula, nombre;
+        
+        //Mensaje que sale en pantalla.
+        Console.Write("\nMatricula: ");
+
+        //Entrada de datos con ReadLine.
+        matricula = Console.ReadLine();
+
+        Console.Write("\nNombre: ");
+        nombre = Console.ReadLine();
+
+        //Salida de datos.
+        Console.WriteLine("\n*** Salida por Consola ***\n");
+        Console.WriteLine("Su nombre es: \t\t" + nombre);
+        Console.WriteLine("Su matricula es: \t" + matricula);
     }
 }
