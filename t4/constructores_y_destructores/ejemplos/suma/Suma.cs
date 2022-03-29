23c28943545887f9a59540da54888d7f676fca13
julcaso <julioenriquecastro@gmail.com>
2022-03-28 21:14:15 -0400

    Create Suma.cs

---
+++ t4/constructores_y_destructores/ejemplos/suma/Suma.cs
@@ -0,0 +1,45 @@
+/***************************************
+Autor:    Profesor Julio Castro Ortiz.
+Fecha:    3/28/2022.
+Semestre: 2021-20.
+Objetivo: Ejemplo docente.
+Tema:     Constructores y destructores.
+Materias: INF512, INF513.
+****************************************/
+
+using System;
+
+class Suma{
+
+    int val1, val2;
+    float val3, val4;
+
+    public Suma(){
+        val1 = 5;
+        val2 = 5;
+        Console.WriteLine("La suma por default es: {0} ", val1 + val2);
+    }
+
+    public Suma(int parVal1, int parVal2){
+        val1 = parVal1;
+        val2 = parVal2;
+        Console.WriteLine("La suma entera es: {0} ", val1 + val2);
+    }
+
+    public Suma(float val3, float val4){
+        this.val3 = val3;
+        this.val4 = val4;
+        Console.WriteLine("La suma flotante es: {0} ", this.val3 + this.val4);
+
+    }
+}
+
+class SumaTest{
+    static void Main(){
+        
+        Suma sm1 = new Suma();
+        Suma sm2 = new Suma(56, 78);
+        Suma sm3 = new Suma(68.0f, 85.4f);
+       
+    }
+}
