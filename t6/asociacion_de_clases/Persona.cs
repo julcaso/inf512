2ef991289447bb49aacff99421fef94d5f955689
Julio Castro Ortiz <julioenriquecastro@gmail.com>
2023-12-07 19:35:56 -0400

    Add files via upload

---
+++ t6/asociacion_de_clases/Persona.cs
@@ -0,0 +1,31 @@
+using System;
+class Computadora{
+    private string modelo;
+    private float precio;
+    
+    public Computadora(){}
+
+    public Computadora(string modelo, float precio){
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+}
+
+class Persona{
+    private int edad;
+    private string nombre, apellido;
+    Computadora comp;
+
+    public Persona{}
+
+    public void asociarComputadora(string modelo, float precio){
+        comp = new Computadora(modelo, precio);
+    }
+}
+
+class PersonaTest{
+    static void Main(){
+        Persona p1 = new Persona();
+        p1.asociarComputadora();
+    }
+}
